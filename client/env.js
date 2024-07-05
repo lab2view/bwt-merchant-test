3894ab2b025590f59c46da50f82fd119a2800536
jspascal <fulgaros@gmail.com>
2024-07-06 00:03:18 +0100

    fix typo in callback url

--- client/env.js
+++ client/env.js
@@ -3,7 +3,7 @@ window.env = {
     "BWT07.01.2019cUIzNnhxeWZvalR5WlpTblJYejN3SGVVbzFWMzlRQ0FacDVQa2wzRmV4T0E1N0tDdkxDYW1OMzh2SVE4a2huV0FTNERsV1BHcVczSQ==",
   applicationSecret:
     "BWT07.01.2019Z1pTbFZ3bXVnZ0ZZWndPWDhkNEJ4ZXluNEg0Y3daTFd6NHVxT3ZDU1pUSFc2VEtueTQyYmFCS0x2OWRNOTZwbkdVbnJCNHZ2VGZzTEtLN0U=",
-  callbackUrl: "https://bwt-merchant-callback.onrender.com/api/callbacks",
+  callbackUrl: "https://bwt-merchant-callback.onrender.com/api/payment",
   successUrl: "https://bwt-merchant-test.onrender.com/success.html",
   cancelUrl: "https://bwt-merchant-test.onrender.com/",
 };
