541b4a4033c89e255148b03a49ea3f77a599b237
jspascal <fulgaros@gmail.com>
2024-07-06 15:14:50 +0100

    implemented validation

--- client/app/controllers/bewallet.ctrl.js
+++ client/app/controllers/bewallet.ctrl.js
@@ -22,9 +22,30 @@ angular
       fail: "fas fa-times",
     };
 
-    $scope.operators = [];
+    this.callbacks = [];
+    this.paginatedCallbacks = [];
+    this.pageSize = 3;
+    this.currentPage = 1;
 
-    $scope.callbacks = [];
+    this.operators = [];
+
+    this.callbacks = [];
+
+    this.updatePagination = function () {
+      this.totalPages = Math.ceil(this.callbacks.length / this.pageSize);
+
+      this.currentPage = Math.min(
+        Math.max(this.currentPage, 1),
+        this.totalPages
+      );
+
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+
+      this.paginatedCallbacks = this.callbacks.slice(
+        startIndex,
+        startIndex + this.pageSize
+      );
+    };
 
     $http
       .get(window.env.callbackUrl + "/api/callbacks", {
@@ -46,6 +67,7 @@ angular
             };
           });
           console.log(this.callbacks);
+          this.updatePagination();
         }.bind(this)
       )
       .catch(function (error) {
@@ -72,12 +94,31 @@ angular
                 status: data.status,
               };
             });
+            this.updatePagination();
             console.log("Callbacks refreshed:", this.callbacks);
+            console.log(
+              "Paginanted callbacks refreshed:",
+              this.paginatedCallbacks
+            );
           }.bind(this)
         )
         .catch(function (error) {
           console.error("Error fetching callbacks:", error);
         });
+    };
+
+    this.prevPage = function () {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.updatePagination();
+      }
+    };
+
+    this.nextPage = function () {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.updatePagination();
+      }
     };
 
     $scope.submit = function () {
