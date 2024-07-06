b9cd00a8b6a1f5729567de40f036a3ab7423ef58
jspascal <fulgaros@gmail.com>
2024-07-06 02:28:42 +0100

    clean

--- client/env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-  applicationId: "BWT07.01.2019cUIzNnhxeWZvalR5WlpTblJYejN3SGVVbzFWMzlRQ0FacDVQa2wzRmV4T0E1N0tDdkxDYW1OMzh2SVE4a2huV0FTNERsV1BHcVczSQ==",
-  applicationSecret: "BWT07.01.2019Z1pTbFZ3bXVnZ0ZZWndPWDhkNEJ4ZXluNEg0Y3daTFd6NHVxT3ZDU1pUSFc2VEtueTQyYmFCS0x2OWRNOTZwbkdVbnJCNHZ2VGZzTEtLN0U=",
-  callbackUrl: "https://bwt-merchant-callback.onrender.com",
-  successUrl: "https://bwt-merchant-test.onrender.com",
-  cancelUrl: "https://bwt-merchant-test.onrender.com",
-  bwtUrl: "https://staging-merchant.be-wallet.net/api/v2",
-};
